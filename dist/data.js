39c6d0b67a658527386828d1f86e2a0a852cd74b
Byprimetaverse <byprimemetavers@gmail.com>
2025-06-15 10:20:14 +0700

    Initial deploy setup

---
+++ dist/data.js
@@ -0,0 +1,21 @@
+import Hero from "@/public/assets/company.webp";
+import Proyek1 from "@/public/assets/proyek1.webp";
+import Proyek2 from "@/public/assets/proyek2.webp";
+import Proyek3 from "@/public/assets/proyek3.webp";
+import Proyek4 from "@/public/assets/proyek4.webp";
+import Proyek5 from "@/public/assets/proyek5.webp";
+import Proyek6 from "@/public/assets/Proyek6.webp";
+import logo1 from "@/public/assets/logo1.png";
+
+const Image = {
+  Hero,
+  Proyek1,
+  Proyek2,
+  Proyek3,
+  Proyek4,
+  Proyek5,
+  Proyek6,
+  logo1,
+};
+
+export default Image;
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
+const isGithubPages = process.env.GITHUB_PAGES === "true";
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  basePath: isGithubPages ? "/website.PSL" : "",
+  assetPrefix: isGithubPages ? "/website.PSL/" : "",
+};
 
 export default nextConfig;
